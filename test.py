f2a2a6b6a495d599edcbbcab247c9243e46d83ad
deonnarHU <cs.molnar92@gmail.com>
2020-11-28 01:25:59 +0100

    Replay logic added

--- test.py
+++ test.py
@@ -8,22 +8,37 @@ led2 = LED(27)
 led3 = LED(22)
 led4 = LED(23)
 
-def func(a, b):
-    print("Called function")
-    return a * b
- 
-# Schedule a timer for 5 seconds
-# We pass arguments 3 and 4
-t = Timer(5.0, func, [3, 4])
- 
-start_time = time.time()
- 
-# Start the timer
-t.start()
- 
-end_time = time.time()
- 
-if end_time - start_time < 5.0:
-    print("Timer will wait for sometime before calling the function")
-else:
-    print("5 seconds already passed. Timer finished calling func()")
+h = [1,3,4,1,1]
+ht = [120,120,780,1780,2780]
+noteStarters = {}
+noteStoppers = {}
+
+def startNote(x):
+    #print("startingu")
+    noteSelector(x).off()
+    return 1
+
+def stopNote(x):
+    #print("stopperino")
+    noteSelector(x).on()
+    return 1
+
+def noteSelector(x):
+    switcher = {
+        1: led1,
+        2: led2,
+        3: led3,
+        4: led4
+        }
+    return switcher.get(x,"Note exist NO")
+
+i = 0
+for x in h:
+    timerOnName = "t" + str(i) + "On"
+    timerOffName = "t" + str(i) + "Off"
+    noteStarters[timerOnName] = Timer(ht[i]/1000, startNote, [x])
+    noteStoppers[timerOffName] = Timer(ht[i]/1000 + 0.1, stopNote, [x])
+    noteStarters[timerOnName].start()
+    noteStoppers[timerOffName].start()
+    i = i + 1
+    
