0a37dc9f4e84109ae0873fed9f7088ee6dbe3237
deonnarHU <cs.molnar92@gmail.com>
2020-11-28 01:51:26 +0100

    Added song read logic

--- test.py
+++ test.py
@@ -2,26 +2,32 @@ from gpiozero import LED
 from time import sleep
 from threading import Timer
 import time
+import json
 
 led1 = LED(17)
 led2 = LED(27)
 led3 = LED(22)
 led4 = LED(23)
 
-h = [1,3,4,1,1]
-ht = [120,120,780,1780,2780]
+#h = [1,3,4,1,1]
+#ht = [120,120,780,1780,2780]
 noteStarters = {}
 noteStoppers = {}
+
+def readSong(songName):
+    with open("song0001.json") as f:
+        data = json.load(f)
+    return data
 
 def startNote(x):
     #print("startingu")
     noteSelector(x).off()
-    return 1
+    return 0
 
 def stopNote(x):
     #print("stopperino")
     noteSelector(x).on()
-    return 1
+    return 0
 
 def noteSelector(x):
     switcher = {
@@ -32,6 +38,11 @@ def noteSelector(x):
         }
     return switcher.get(x,"Note exist NO")
 
+
+#Load Singysong
+song = readSong("song0001.json")
+h = song.noteList
+ht = song.noteTime
 i = 0
 for x in h:
     timerOnName = "t" + str(i) + "On"
