bc96cb6fcd6f70146205f116e4afd5adfecfa24b
deonnarHU <cs.molnar92@gmail.com>
2020-11-30 21:04:03 +0100

    Added schedule example

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,22 @@
 import playListPlayer
+import schedule
+import time
 
 print("suttyóság")
 
-playListPlayer.playPlaylist("playlist0001.json")
+
+def job():
+    print("I'm working...")
+
+schedule.every().minutes.do(job)
+#schedule.every().hour.do(job)
+#schedule.every().day.at("10:30").do(job)
+#schedule.every(5).to(10).minutes.do(job)
+#schedule.every().monday.do(job)
+#schedule.every().wednesday.at("13:15").do(job)
+#schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+#playListPlayer.playPlaylist("playlist0001.json")
