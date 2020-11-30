fc8c30bd2c87dd6160ea4559ad0e5aab86366309
deonnarHU <cs.molnar92@gmail.com>
2020-11-30 15:53:04 +0100

    syntax fix

--- songPlayer.py
+++ songPlayer.py
@@ -52,8 +52,8 @@ def playSong(songTitle):
     for x in h:
         timerOnName = "t" + str(i) + "On"
         timerOffName = "t" + str(i) + "Off"
-        noteStarters[timerOnName] = Timer(ht[i]/1000, startNote, [x])
-        noteStoppers[timerOffName] = Timer(ht[i]/1000 + 0.1, stopNote, [x])
+        noteStarters[timerOnName] = Timer(ht(i)/1000, startNote, [x])
+        noteStoppers[timerOffName] = Timer(ht(i)/1000 + 0.1, stopNote, [x])
         noteStarters[timerOnName].start()
         noteStoppers[timerOffName].start()
         i = i + 1
